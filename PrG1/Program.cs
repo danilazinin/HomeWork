8797e0b4cfc086b08913c86127e44320c73729c1
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-08-30 22:53:11 +0300

    задание 2

---
+++ PrG1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a > b) 
+{
+     max = a;
+     min = b;
+}
+else 
+{
+     max = b;
+     min = a;
+}
+Console.WriteLine($"макс = {max}, минимальное = {min}");
