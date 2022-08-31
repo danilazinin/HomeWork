a4ed0d6cf1723608874e8d57652fa296cbf01a82
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-08-31 08:51:46 +0300

    Задача 6

---
+++ PrG3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Это четное");
+}
+else
+{
+    Console.WriteLine("Это НЕчетное");
+}
