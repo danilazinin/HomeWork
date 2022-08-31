eda8c2cc34b236d0acc15a958f96e370f88593c7
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-08-31 09:02:26 +0300

    Задание 8

---
+++ PrG4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+while (i > 0 && i <= a)
+{
+    if ((i % 2) == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i = i + 1;
+}
